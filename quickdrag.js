0a539ad536281b15a26e7ba045595303c2782394
vdegenne <vdegenne@gmail.com>
2019-03-20 15:40:34 +0100

    wait 2s on load

--- quickdrag.js
+++ quickdrag.js
@@ -1,45 +1,45 @@
-const draggables = [];
-const offset = {};
+const draggables = []
+const offset = {}
 
-export const registerForDraggable = (element) => {
+export const registerForDraggable = element => {
   if (draggables.includes(element)) {
-    console.warn('already registered element');
-    return;
+    console.warn('already registered element')
+    return
   }
-  draggables.push(element);
+  draggables.push(element)
 
-  element.addEventListener('mousedown', async (e) => {
-    element.setAttribute('dragged', '');
-    element.style.userSelect = 'none';
-    offset.x = e.offsetX;
-    offset.y = e.offsetY;
+  element.addEventListener('mousedown', async e => {
+    element.setAttribute('dragged', '')
+    element.style.userSelect = 'none'
+    offset.x = e.offsetX
+    offset.y = e.offsetY
     if (e.path[0] !== element) {
-      offset.x += e.path[0].offsetLeft;
-      offset.y += e.path[0].offsetTop;
+      offset.x += e.path[0].offsetLeft
+      offset.y += e.path[0].offsetTop
     }
-  });
+  })
 }
 
-window.addEventListener('mousemove', (e) => {
-  const current = draggables.filter(d => d.hasAttribute('dragged'))[0];
+window.addEventListener('mousemove', e => {
+  const current = draggables.filter(d => d.hasAttribute('dragged'))[0]
   if (current) {
-    current.style.position = 'absolute';
-    current.style.top = `${e.y - offset.y}px`;
-    current.style.left = `${e.x - offset.x}px`;
+    current.style.position = 'absolute'
+    current.style.top = `${e.y - offset.y}px`
+    current.style.left = `${e.x - offset.x}px`
   }
-});
+})
 
 window.addEventListener('mouseup', () => {
-  const current = draggables.filter(d => d.hasAttribute('dragged'))[0];
+  const current = draggables.filter(d => d.hasAttribute('dragged'))[0]
   if (current) {
-    current.removeAttribute('dragged');
-    current.style.userSelect = 'initial';
+    current.removeAttribute('dragged')
+    current.style.userSelect = 'initial'
   }
-});
+})
 
-
-window.addEventListener('load', () => {
-  for (const el of document.body.querySelectorAll('.draggable')) {
-    registerForDraggable(el);
+window.addEventListener('load', async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  for (let el of document.body.querySelectorAll('.draggable')) {
+    registerForDraggable(el)
   }
-});
+})
